b92ac111667aa1eadb6b4aeccab706b076184ab9
Gabor Csardi <csardi.gabor@gmail.com>
2015-07-18 17:36:41 +0000

    version 2.0.0

--- R/getset.R
+++ R/getset.R
@@ -7,6 +7,21 @@ config <- new.env()
 
 #' Query a configuration parameter key
 #'
+#' Query a configuration parameter key, and return the value
+#' set in the calling package(s).
+#'
+#' @details
+#' This function is meant to be called from the package whose
+#' behavior depends on it. It searches for the given configuration
+#' key, and if it exists, it checks which package(s) it was called
+#' from and returns the configuration setting for that package.
+#'
+#' If the key is not set in any calling package, but it is set in
+#' the global environment (i.e. at the R prompt), then it returns that
+#' setting.
+#'
+#' If the key is not set anywhere, then it returns \code{NULL}.
+#'
 #' @param key The name of the parameter to query.
 #' @param fallback Fallback if the parameter id not found anywhere.
 #' @return The value of the parameter, or the fallback value if not found.
@@ -20,7 +35,7 @@ get_config <- function(key, fallback = NULL) {
 
 get_from_session <- function(key) {
   value <- config[[key]]
-  if (is.null(value)) return(list(NULL))
+  if (is.null(value)) return(NULL)
 
   pkgs <- sys.frames()
   pkgs <- lapply(pkgs, parent.env)
@@ -29,7 +44,11 @@ get_from_session <- function(key) {
   pkgs <- unique(pkgs)
 
   for (p in rev(pkgs)) {
-    if (p %in% names(value)) return(value[[p]])
+    if (p %in% names(value)) return(value[p])
+  }
+
+  if ("R_GlobalEnv" %in% names(value)) {
+    return(value["R_GlobalEnv"])
   }
 
   NULL
@@ -39,15 +58,18 @@ get_from_session <- function(key) {
 
 #' Set a configuration parameter
 #'
+#' Set a configuration parameter, for the package we are calling from.
+#' If called from the R prompt and not from a package, then it sets
+#' the parameter for global environment.
+#'
 #' @param ... Parameters to set, they should be all named.
 #' @return Nothing.
 #'
 #' @export
-#' @importFrom utils packageName
+#' @seealso \code{\link{set_config_in}}
 
 set_config <- function(...) {
-  check_named_args(...)
-  set_config_session(who = packageName(env = parent.frame()), ...)
+  set_config_in(..., .in = parent.frame())
 }
 
 check_named_args <- function(...) {
@@ -55,6 +77,32 @@ check_named_args <- function(...) {
   if (is.null(nn) || any(nn == "")) {
     stop("Some parameters are not named")
   }
+}
+
+#' Set a configuration parameter for a package
+#'
+#' This is a more flexible variant of \code{link{set_config}},
+#' and it allows creating an custom API in the package that
+#' uses \code{pkgconfig} for its configuration.
+#'
+#' @details
+#' This function is identical to \code{\link{set_config}}, but it allows
+#' supplying the environment that is used as the package the configuration
+#' is set for. This makes it possible to create an API for setting
+#' (and getting) configuration parameters.
+#'
+#' @param ... Parameters to set, they should be all named.
+#' @param .in An environment, typically belonging to a package.
+#' @return Nothing.
+#'
+#' @export
+#' @seealso \code{\link{set_config}}
+#' @importFrom utils packageName
+
+set_config_in <- function(..., .in = parent.frame()) {
+  check_named_args(...)
+  who <- packageName(env = .in) %||% "R_GlobalEnv"
+  set_config_session(who = who, ...)
 }
 
 set_config_session <- function(who, ...) {
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -21,6 +21,27 @@ test_that("Session variables", {
 
 })
 
+
+test_that("Composite values", {
+
+  on.exit(try(disposables::dispose_packages(pkgs)))
+  pkgs <- disposables::make_packages(
+    pkgconfigtest = {
+      f <- function() {
+        set_config(foo = list(1,2,3))
+        get_config("foo")
+      }
+      g <- function() { get_config("foo") }
+      h <- function() { get_config("foobar") }
+    }
+  )
+
+  expect_equal(f(), list(1,2,3))
+  expect_equal(g(), list(1,2,3))
+  expect_null(h())
+
+})
+
 context("Keys are private")
 
 test_that("Two packages do not interfere", {
@@ -72,9 +93,12 @@ test_that("Cannot get if set by another package", {
 
 test_that("Setting from .onLoad works fine", {
 
-  on.exit(try(disposables::dispose_packages(pkgs)))
+  on.exit(try(disposables::dispose_packages(pkgs)), add = TRUE)
 
   pkgs <- disposables::make_packages(
+
+    lib_dir = .libPaths()[1],
+    quiet = FALSE,
 
     utility = {
       getter <- function() { pkgconfig::get_config("key", "fallback") }
